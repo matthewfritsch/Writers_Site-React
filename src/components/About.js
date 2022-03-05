f982bfd819e64b6cb2fb9f192d59f672d18d5436
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-03-04 19:57:21 -0800

    Added about component with Harold content

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,36 @@
-import './styling.css'
+import {
+  Box,
+  Center,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 const About = () => {
-  return (
-    <div className='containers'>
-      <h4>About</h4>
-    </div>
-  )
-}
+  let aboutImg = require("../assets/harold/harold_about.jpg");
 
-export default About
+  return (
+    <>
+      <Center my={10}>
+        <Heading>about</Heading>
+      </Center>
+      <Stack justifyContent="center" direction="row" spacing={10}>
+        <Box my={50} w={300} fontFamily="Jost">
+          <Text my={3}>Hi! I am a subject of stock images.</Text>
+          <Text my={3}>
+            I collect old computers/laptops, and make my own ceramic mugs for
+            coffee ☕ I love working with my hands to create something new, or
+            repair integrated hardware 🖥️
+          </Text>
+          <Text>Thanks for visiting my site!</Text>
+        </Box>
+        <Image w={500} borderRadius={4} src={aboutImg}></Image>
+      </Stack>
+    </>
+  );
+};
+
+export default About;
