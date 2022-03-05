6bf30b7dacd1e3ac7e88c6ed644f07c192c37ea6
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-03-04 20:47:35 -0800

    added PubItem as a component to show publication information

---
+++ src/components/PubItem.js
@@ -0,0 +1,30 @@
+import { Center, Container, Flex, Link, Text } from "@chakra-ui/react";
+
+const PubItem = ({ title, titleLink, tech, techLink, addNote, bottomNote }) => {
+  return (
+    <Container w="container.sm" mb={5}>
+      <Flex>
+        {titleLink.length > 0 ? (
+          <Link isExternal href={"https://" + titleLink}>
+            {title}
+          </Link>
+        ) : (
+          <Text>{title}</Text>
+        )}
+        <Text mx={2}>-</Text>
+        {techLink.length > 0 ? (
+          <Link isExternal href={"https://" + techLink}>
+            {tech}
+          </Link>
+        ) : (
+          <Text>{tech}</Text>
+        )}
+        <Text>{addNote && ","}</Text>
+        <Text mx={2}>{addNote}</Text>
+      </Flex>
+      <Text>{bottomNote}</Text>
+    </Container>
+  );
+};
+
+export default PubItem;
