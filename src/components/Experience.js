64ab930e19c9a42fee098b41ec3f2d85c32389b5
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-03-04 19:58:53 -0800

    Reformatted ExperienceBox structure, switched theme props to theme.js defaults

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,36 +1,125 @@
-import { Box, Center, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import ExperienceBox from './ExperienceBox';
+import {
+  Box,
+  Center,
+  Heading,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import ExperienceBox from "./ExperienceBox";
+
+const prevExp = [
+  {
+    title: "undergrad",
+    workName: "uc santa cruz",
+    workLink: "www.ucsc.edu/",
+    timeframe: "Sep 2019 - Dec 2021",
+  },
+  {
+    title: "programming instructor",
+    workName: "the coder school",
+    workLink: "www.thecoderschool.com/locations/pleasanton/",
+    timeframe: "Aug 2017 - Mar 2021",
+  },
+  {
+    title: "software engineer intern",
+    workName: "carl zeiss meditec",
+    workLink: "zeiss.com",
+    timeframe: "Jun 2021 - Aug 2021",
+  },
+];
+
+const currExp = [
+  {
+    title: "applying for jobs",
+    workName: "everywhere",
+    workLink: "www.youtube.com/watch?v=bx3--22D4E4",
+    timeframe: "Jan 2022 - present",
+  },
+  {
+    title: "learning ReactJS",
+    workName: "at my desk",
+    workLink: "reactjs.org/",
+    timeframe: "Feb 2022 - present",
+  },
+  {
+    title: "learning Rust",
+    workName: "at my desk",
+    workLink: "www.rust-lang.org/",
+    timeframe: "Jan 2022 - present",
+  },
+];
+
+// future experiences, if any
+const futuExp = [
+  {
+    title: "what am I, a fortune teller?",
+    workName: "🤡",
+    workLink: "www.youtube.com/watch?v=CJo1-VJzwY4",
+    timeframe: "??? - ???",
+  },
+];
 
 const Experience = () => {
-
+  const maxExpLen = Math.max(prevExp.length, currExp.length);
+  const tabSize = 240;
   const tabListSize = 720;
-  const tabSize = tabListSize/3;
+  const tabListHeight = 120;
 
   return (
-    <Center>
-      {/* <h4>Experience</h4> */}
-      <Tabs>
-        <TabList width={tabListSize}>
-          <Tab width={tabSize}>Past</Tab>
-          <Tab width={tabSize}>Current</Tab>
-          <Tab width={tabSize}>Future</Tab>
-        </TabList>
+    <>
+      <Center mb={4}>
+        <Heading>school/work experience</Heading>
+      </Center>
+      <Center fontFamily="Jost">
+        <Tabs defaultIndex={1}>
+          <TabList width={tabListSize}>
+            <Tab width={tabSize}>Past</Tab>
+            <Tab width={tabSize}>Current</Tab>
+            <Tab width={tabSize}>Future</Tab>
+          </TabList>
 
-        <TabPanels minHeight={200}>
-          <TabPanel>
-            <ExperienceBox title='Software Engineer' workName='Stripe'/>
-            <ExperienceBox />
-          </TabPanel>
-          <TabPanel>
-          <ExperienceBox title='Software Engineer' workName='Stripe'/>
-          </TabPanel>
-          <TabPanel>
-          <ExperienceBox title='Software Engineer' workName='Stripe' workLink='stripe.com'/>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Center>
-  )
-}
+          <TabPanels minHeight={200}>
+            <TabPanel>
+              {prevExp.map((exp, index) => (
+                <ExperienceBox
+                  key={index}
+                  title={exp.title}
+                  workName={exp.workName}
+                  workLink={exp.workLink}
+                  timeframe={exp.timeframe}
+                />
+              ))}
+            </TabPanel>
+            <TabPanel>
+              {currExp.map((exp, index) => (
+                <ExperienceBox
+                  key={index}
+                  title={exp.title}
+                  workName={exp.workName}
+                  workLink={exp.workLink}
+                  timeframe={exp.timeframe}
+                />
+              ))}
+            </TabPanel>
+            <TabPanel>
+              {futuExp.map((exp, index) => (
+                <ExperienceBox
+                  key={index}
+                  title={exp.title}
+                  workName={exp.workName}
+                  workLink={exp.workLink}
+                  timeframe={exp.timeframe}
+                />
+              ))}
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Center>
+    </>
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/ExperienceBox.js
+++ src/components/ExperienceBox.js
@@ -1,19 +1,31 @@
-import { Center, Container, Flex, Link, Text } from "@chakra-ui/react"
+import { Center, Container, Flex, Link, Text } from "@chakra-ui/react";
 
+const ExperienceBox = ({ title, workName, workLink, timeframe }) => {
+  const width = 560;
 
-const ExperienceBox = ({title, workName, workLink, timeframe}) => {
-
-    const width = 560;
-
-  return ( !title || !workName || !workLink ? <></> : (
-    <Container w={width} borderRadius={8} padding={3} border='2px' borderColor='#749ECB' display='flex' flexDir='column' justifyContent='center' flex minH={30}>
-        <Flex flexDir='row'>
-            <Text marginRight={2} textStyle='underline'>{title}</Text>
-            <Link color='blue.400' href={'http://'+workLink}>@{workName}</Link>
-        </Flex>
-        <Text>{timeframe}</Text>
+  return !title || !workName || !workLink ? (
+    <></>
+  ) : (
+    <Container
+      w={width}
+      borderRadius={8}
+      padding={3}
+      border="2px"
+      marginBottom={1}
+      borderColor="#749ECB"
+      justifyContent="center"
+    >
+      <Flex>
+        <Text marginRight={2} textStyle="underline">
+          {title}
+        </Text>
+        <Link color="blue.400" href={"http://" + workLink}>
+          @{workName}
+        </Link>
+      </Flex>
+      <Text>{timeframe}</Text>
     </Container>
-  ))
-}
+  );
+};
 
-export default ExperienceBox
+export default ExperienceBox;
