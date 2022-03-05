c33d522960788bf9aa582ba8d020182fa1d644f5
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-03-04 19:59:20 -0800

    Added footer content, referenced the original creator (Thanks Star!)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,31 @@
-import './styling.css'
-
+import { Center, Link, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <div className='containers'>
-    <h4>Footer</h4>
-  </div>
-  )
-}
+    <>
+      <Center py={10}>
+        <Link
+          color="gray.600"
+          href="https://matthewfritsch.com"
+          _hover={{ color: "blue.400" }}
+          isExternal
+        >
+          Built by Matthew Fritsch to learn React JS
+        </Link>
+        <Text color="gray.500" mx={2}>
+          |
+        </Text>
+        <Link
+          color="gray.600"
+          href="https://starcsu.com/"
+          _hover={{ color: "blue.400" }}
+          isExternal
+        >
+          Made as a recreation of this site, by Star Su
+        </Link>
+      </Center>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
