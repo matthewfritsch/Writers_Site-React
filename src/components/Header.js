ddcd0053204a1fa2b67135ec339aeede7fa05a94
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-03-04 19:59:59 -0800

    Added dark theme button, switched image for Harold, fixed text formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,52 @@
-import { Box, Center, Container, Heading, Image, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  IconButton,
+  Image,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 const Header = () => {
-  let welcomeImg = require('../assets/welcome_man.png')
+  let welcomeImg = require("../assets/harold/harold_welcome.jpg");
+
+  const { toggleColorMode } = useColorMode();
+
+  const icon = useColorModeValue(<SunIcon />, <MoonIcon />);
 
   return (
-    <Center my={50} display='flex' alignItems='center' justifyContent='center'>
-      <Image height={600} src={welcomeImg} fallback='https://via.placeholder.com/390x600'/>
+    <>
+      <Flex justifyContent="flex-end">
+        <IconButton my={4} icon={icon} onClick={toggleColorMode} pos="fixed" />
+      </Flex>
+      <Center
+        pt={20}
+        pb={10}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Image
+          height={400}
+          src={welcomeImg}
+          fallback="https://via.placeholder.com/390x600"
+          mx={30}
+          borderRadius={5}
+        />
 
-      <Box borderLeft={20}>
-         <Heading> hello, my name is blank </Heading>
-         <Text letterSpacing={2} color="gray">lorem ipsum dolor sit amet</Text>
-      </Box>
-     </Center>
-  )
-}
+        <Box borderLeft={20}>
+          <Heading> hello, my name is harold </Heading>
+          <Text letterSpacing={2} color="gray">
+            stock image subject and internet icon
+          </Text>
+        </Box>
+      </Center>
+    </>
+  );
+};
 
-export default Header
+export default Header;
