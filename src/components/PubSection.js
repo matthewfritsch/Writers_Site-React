516f3a027aa7bec234ffb8df9b52b17f56d92c48
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-03-04 20:48:10 -0800

    Added a component for sections of publications, given not every publication is the same kind of writing

---
+++ src/components/PubSection.js
@@ -0,0 +1,43 @@
+import {
+  Center,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+} from "@chakra-ui/react";
+import PubItem from "./PubItem";
+
+const PubSection = ({ sectName, entries }) => {
+  return (
+    <>
+      <Center
+        display="flex"
+        alignItems="center"
+        position="relative"
+        fontFamily="Jost"
+      >
+        <Grid templateColumns="repeat (5, 1fr)">
+          <GridItem mr={10}>
+            <Text mx={30}>{sectName}</Text>
+          </GridItem>
+          <GridItem colStart={2} colEnd={4}>
+            {entries.map((entry, index) => (
+              <PubItem
+                key={index}
+                title={entry.title}
+                titleLink={entry.titleLink}
+                tech={entry.tech}
+                techLink={entry.techLink}
+                addNote={entry.addNote}
+                bottomNote={entry.bottomNote}
+              />
+            ))}
+          </GridItem>
+        </Grid>
+      </Center>
+    </>
+  );
+};
+
+export default PubSection;
